27eec3b85c28a11ffe9c748134a8bee6ac109bcb
James Hannah <git@james.jph.im>
2021-08-30 18:30:10 +0100

    Updates

--- mqtt.py
+++ mqtt.py
@@ -1,83 +1,94 @@
 #!/usr/bin/env python3
-import time
+import time,json
 from inky import InkyPHAT
 from font_fredoka_one import FredokaOne
 from io import BytesIO
 from PIL import Image,ImageDraw,ImageFont
+import logging
 import requests
 import sys
 from paho.mqtt import client as mqtt_client
 
-broker = '10.100.100.210'
 
-def client_id() -> str:
-    return open('/etc/machine-id').read().strip()
+class Displayer:
+    broker = '10.100.100.210'
+    lasthash = None
+    ph = InkyPHAT('red')
 
-def client_topic() -> str:
-    cid = client_id()
-    return f"phat/client/{cid}"
+    def client_id(self) -> str:
+        return open('/etc/machine-id').read().strip()
 
-def update_state(status: str):
-    client.publish(client_topic(), payload=status, qos=1, retain=True)
+    def client_topic(self) -> str:
+        cid = self.client_id()
+        return f"phat/client/{cid}"
 
-def on_connect(client,userdata,flags,rc):
-    print("connected")
-    update_state("ALIVE")
-    client.subscribe("phat/image",1)
-    client.subscribe(f"phat/image/{client_id()}",1)
-    print("setup done")
+    def update_state(self,status: str):
+        client.publish(self.client_topic(), payload=status, qos=1, retain=True)
 
+    def on_connect(self,client,userdata,flags,rc):
+        logging.info("connected")
+        self.update_state("ALIVE")
+        client.subscribe("phat/image",1)
+        client.subscribe(f"phat/image/{self.client_id()}",1)
+        logging.info("setup done")
 
-ph = InkyPHAT('red')
+    def epaper_display_image(self,img):
+        self.ph.set_image(img)
+        self.ph.show()
 
-def epaper_display_image(img):
-    ph.set_image(img)
-    ph.show()
+    def epaper_display_error(self,msg):
+        im = Image.new("P",(212,104))
+        err_image = ImageDraw.Draw(im)
+        font = ImageFont.truetype(FredokaOne, 22)
+        err_image.text((20,20), msg, self.ph.RED, font)
+        self.epaper_display_image(im)
 
-def epaper_display_error(msg):
-    im = Image.new("P",(212,104))
-    err_image = ImageDraw.Draw(im)
-    font = ImageFont.truetype(FredokaOne, 22)
-    err_image.text((20,20), msg, ph.RED, font)
-    epaper_display_image(im)
+    def on_message(self,client, userdata, message):
+        logging.debug(f"{message.topic}--{str(message.payload.strip())}")
+        try:
+            data = json.loads(message.payload.strip())
+        except:
+            logging.error(sys.exc_info())
+            self.epaper_display_error(f"BADJSON")
+            return
+        if self.lasthash == data['hash']:
+            logging.info('skipping same image update')
+            return
+        self.lasthash = data['hash']
+        rqh = {
+            "Accept": "image/*"
+        }
+        resp = requests.get(data["url"],headers=rqh,stream=True)
+        rspct = resp.headers["Content-Type"]
+        if not rspct.startswith("image/"):
+            self.epaper_display_error(f"resp wasn't image, ignoring: {rspct}")
+        #logging.info(resp.headers)
+        img = Image.open(BytesIO(resp.content))
+        if img.size != (212,104):
+            self.epaper_display_error("image size is incorrect!")
+        self.epaper_display_image(img)
 
-def on_message(client, userdata, message):
-    cm = message.payload.decode('utf-8').strip()
-    if not cm.startswith("http"):
-        epaper_display_error(f"Received message, but it's not a URL: {cm}")
-    else:
-        print("url received")
-    rqh = {
-        "Accept": "image/*"
-    }
-    resp = requests.get(cm,headers=rqh,stream=True)
-    rspct = resp.headers["Content-Type"]
-    if not rspct.startswith("image/"):
-        epaper_display_error(f"resp wasn't image, ignoring: {rspct}")
-    print(resp.headers)
-    img = Image.open(BytesIO(resp.content))
-    if img.size != (212,104):
-        epaper_display_error("image size is incorrect!")
-    epaper_display_image(img)
+    def on_disconnect(self,client, userdata, rc):
+        self.epaper_display_error(f"disconnected rc:{rc}")
 
-def on_disconnect(client, userdata, rc):
-    epaper_display_error(f"disconnected rc:{rc}")
+    def debug_mqtt(self,client, userdata, level, buf):
+        logging.debug(f"{level}: {buf}",file=sys.stderr)
 
-def debug_mqtt(client, userdata, level, buf):
-    print(f"{level}: {buf}",file=sys.stderr)
-
+logging.basicConfig(level=logging.INFO)
 if __name__ == '__main__':
-    client = mqtt_client.Client(client_id())
-    client.on_connect = on_connect
-    client.on_log = debug_mqtt
-    client.on_message = on_message
-    client.will_set(client_topic(), payload="DEAD", qos=1, retain=True)
-    client.connect(broker,1883)
+    d = Displayer()
+    client = mqtt_client.Client(d.client_id())
+    client.on_connect = d.on_connect
+    client.on_log = d.debug_mqtt
+    client.enable_logger()
+    client.on_message = d.on_message
+    client.will_set(d.client_topic(), payload="DEAD", qos=1, retain=True)
+    client.connect(d.broker,1883)
     client.loop_start()
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
-        update_state("SHUTDOWN")
+        d.update_state("SHUTDOWN")
         client.disconnect()
-        print("bye")
+        logging.error("bye")
